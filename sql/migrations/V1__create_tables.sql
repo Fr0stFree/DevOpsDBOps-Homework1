f99d47a80c2335c31619c4825bc20f700ebfa713
krasov <me@frostfree.ru>
2025-03-01 22:30:58 +0300

    update migration

--- sql/migrations/V1__create_tables.sql
+++ sql/migrations/V1__create_tables.sql
@@ -1,17 +1,24 @@
-CREATE TABLE product (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    price FLOAT,
-    picture_url VARCHAR(255)
-);
-CREATE TABLE orders (
-    id SERIAL PRIMARY KEY,
-    status VARCHAR(255),
-    date_created DATE DEFAULT CURRENT_DATE
-);
 CREATE TABLE order_product (
-    id SERIAL PRIMARY KEY,
     order_id INTEGER REFERENCES orders(id),
     product_id INTEGER REFERENCES product(id),
     quantity INTEGER NOT NULL
+);
+CREATE TABLE orders (
+    id SERIAL PRIMARY KEY,
+    status VARCHAR(255)
+);
+CREATE TABLE orders_date (
+    order_id INTEGER REFERENCES orders(id),
+    status VARCHAR(255),
+    date_created DATE DEFAULT CURRENT_DATE
+)
+CREATE TABLE product (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    picture_url VARCHAR(255)
+);
+CREATE TABLE product_info (
+    product_id INTEGER REFERENCES product(id),
+    name VARCHAR(255) NOT NULL,
+    price FLOAT
 );
